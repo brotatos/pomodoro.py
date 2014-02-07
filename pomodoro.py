b200120236ef08b270495ba88a3d56255e5e11b1
Robin Choudhury <xrobinc@gmail.com>
2014-02-07 15:24:47 -0800

    Pomodoro: Update current time and time left at proper points.

--- pomodoro.py
+++ pomodoro.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import time
+from time import sleep
+from math import floor
 from datetime import datetime, timedelta
 import sys
 
@@ -19,32 +20,6 @@ class Pomodoro(object):
         self.SHORT_BREAK = 5
         self.LONG_BREAK = 15
 
-    def calculate_time_left(self):
-        self.time_left = self.end_time - self.current_time
-
-    def run_duration(self, number_of_minutes):
-        time_duration = timedelta(minutes=number_of_minutes)
-        self.end_time = self.current_time + time_duration
-        self.calculate_time_left()
-        self.countdown()
-
-    def countdown(self):
-        while self.time_left.total_seconds() > 0:
-            self.update_times()
-            self.print_time_left()
-            self.alert_end_of_period()
-
-    def print_time_left(self):
-        sys.stdout.write("\r" + str(self.end_time - self.current_time))
-        sys.stdout.flush()
-
-    def alert_end_of_period(self):
-        if self.time_left.total_seconds() <= 0:
-            # Play a sound file here.
-            print("\n\aDone!")
-        else:
-            time.sleep(1)
-
     def do_pomodoro(self):
         self.run_duration(number_of_minutes=self.POMODORO_LENGTH)
 
@@ -54,18 +29,54 @@ class Pomodoro(object):
     def take_short_break(self):
         self.run_duration(number_of_minutes=self.SHORT_BREAK)
 
-    def get_times(self):
-        return {
-            'current_time':  self.current_time,
-            'end_time':      self.end_time,
-            'time_left':     self.time_left
-        }
+    def run_duration(self, number_of_minutes):
+        time_duration = timedelta(minutes=number_of_minutes)
+        self.update_current_time()
+        self.end_time = self.current_time + time_duration
+        self.countdown()
+
+    def countdown(self):
+        self.update_time_left()
+        while self.time_left.total_seconds() > 0:
+            self.print_time_left()
+            self.alert_end_of_period()
+
+    def print_time_left(self):
+        if self.time_left.total_seconds() > 0:
+            self.update_current_time()
+            sys.stdout.write("\r" + str(self.end_time - self.current_time))
+            sys.stdout.flush()
+
+    def alert_end_of_period(self):
+        self.update_times()
+        if self.time_left.total_seconds() <= 0:
+            # Play a sound file here.
+            print("\n\aDone!")
+        else:
+            sleep(1)
 
     def update_times(self, end_time=None):
+        self.update_current_time()
+        self.update_time_left()
+
+    def update_current_time(self):
         self.current_time = datetime.now()
+
+    def calculate_time_left(self):
+        self.time_left = self.end_time - self.current_time
+
+    def update_time_left(self, end_time=None):
         if self.end_time:
             self.calculate_time_left()
         elif end_time:
             self.time_left = end_time - self.current_time
         else:
             raise PomodoroException("end_time was not specified.")
+
+    @property
+    def get_times(self):
+        return {
+            'current_time':  self.current_time,
+            'end_time':      self.end_time,
+            'time_left':     self.time_left
+        }
