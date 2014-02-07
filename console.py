7fddf2a3fa47153ab5158ce864d44f02e5cc8817
Robin Choudhury <xrobinc@gmail.com>
2014-02-07 15:24:27 -0800

    Console: Add question and options.

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 from pomodoro import Pomodoro
 
 
@@ -8,7 +9,23 @@ class Console(object):
         self.pomodoro = Pomodoro()
 
     def ask_question(self):
-        print("(p)omodoro, (l)ong break, (s)hort break")
+        while True:
+            print("(p)omodoro, (l)ong break, (s)hort break, (q)uit")
+            response = input("> ")
+            self.do_pomodoro_action(response)
+
+    def do_pomodoro_action(self, letter):
+        if letter == 'p':
+            self.pomodoro.do_pomodoro()
+        elif letter == 'l':
+            self.pomodoro.take_long_break()
+        elif letter == 's':
+            self.pomodoro.take_short_break()
+        elif letter == 'q':
+            sys.exit(0)
+        else:
+            print("Type p, l, or s to select our choice.")
+            self.ask_question()
 
 
 console = Console()
